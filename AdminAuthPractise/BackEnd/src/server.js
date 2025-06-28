e684e9f1c744f0fe1861897009b96fb64e5e1523
ShubhamBisht18 <155867546+ShubhamBisht18@users.noreply.github.com>
2025-06-28 21:02:24 +0530

    Backend of admin authentication

---
+++ AdminAuthPractise/BackEnd/src/server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config({
+    path: './src/.env'
+})
+
+import authRoutes from './routes/authRoutes.js'
+
+const port = process.env.PORT || 3000
+const app = express()
+
+// MiddleWare
+app.use(cors({origin: process.env.CLIENT_URL,credentials: true}))
+app.use(express.json())
+app.use(cookieParser())
+
+// Routes
+app.use('/api/auth',authRoutes)
+
+// Mongoose Connnectivity
+const startServer = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`✅ Connected to MongoDB: ${mongoose.connection.name}`)
+
+        app.listen(port, () =>{
+            console.log(`🚀 Server is running at http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.error('❌ MongoDB connection error:', error.message);
+        process.exit(1);
+    }
+}
+
+
+startServer()
